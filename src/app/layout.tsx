39d8014a6e0f875548998524cd14a6d6fe494c48
Nikita <nikita@MacBook-Pro-Nikita.local>
2023-04-21 14:51:31 +0600

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import Providers from "@/components/Providers";
+import { cn } from "../lib/utils";
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/Toast";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html
+            lang="en"
+            className={cn(
+                "bg-white text-slate-900 antialiased",
+                inter.className
+            )}
+        >
+            <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
+                <Providers>
+                    {children}
+
+                    {/* @ts-expect-error Server Component */}
+                    <Navbar />
+                </Providers>
+
+                <Toaster position="bottom-center" />
+
+                {/* Allow for more height on mobile devices */}
+                <div className="h-40 md:hidden" />
+            </body>
+        </html>
+    );
+}
